966aac0af9a41dd648920a7c7cbd346f090ce516
Wojciech Zielonka <wojtas.zielonka@gmail.com>
2022-04-27 11:11:23 +0200

    Arcface image input comments

--- gdl/models/mica/mica.py
+++ gdl/models/mica/mica.py
@@ -1,14 +1,18 @@
 import os
 
+import cv2
 import torch
 import torch.nn.functional as F
+from config import get_cfg_defaults
+from insightface.utils import face_align
 from loguru import logger
 from torch import nn
 
-
-from config import get_cfg_defaults
 from mica.arcface import ArcfaceMica
 from mica.decoder import DecoderMica
+
+input_mean = 127.5
+input_std = 127.5
 
 
 class Mica(nn.Module):
@@ -42,17 +46,39 @@ class Mica(nn.Module):
             'decoder': self.decoder.state_dict()
         }
 
-    def run(self, images):
+    def get_arcface_input(self, img, lmks):
+        aimg = face_align.norm_crop(img, landmark=lmks)
+        blob = cv2.dnn.blobFromImages([aimg], 1.0 / input_std, (112, 112), (input_mean, input_mean, input_mean), swapRB=True)
+        return blob[0], aimg
+
+    def crop_images(self, images, lmks):
+        # Keypoint needed for arcface image processing pipeline:
+
+        # "right_eye"
+        # "left_eye"
+        # "nose"
+        # "mouth_right"
+        # "mouth_left"
+
+        # images (H, W, 3)
+
+        arcface_input = self.get_arcface_input(images, lmks)
+
+        # Load to cuda and transform to tensor?
+
+        return arcface_input
+
+    def run(self, images, lmks):
         """
         Runs MICA network and return FLAME vertices
         :param images: input images
         :return: 3D vertices
         """
-        identity_code = F.normalize(self.encoder(images))
-        vertices, code = self.decoder(identity_code)
+        identity_code = F.normalize(self.encoder(self.crop_images(images, lmks)))
+        vertices, shape_code = self.decoder(identity_code)
 
         return {
             'identity_code': identity_code,
-            'shape_code': code,
+            'shape_code': shape_code,
             'vertices': vertices
         }
