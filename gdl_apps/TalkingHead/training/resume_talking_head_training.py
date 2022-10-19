2c358073a604bd3152c96c32fe53521896031a62
rdanecek <danekradek@gmail.com>
2022-10-19 11:02:55 +0200

    Clean some old comments

--- gdl_apps/TalkingHead/training/resume_talking_head_training.py
+++ gdl_apps/TalkingHead/training/resume_talking_head_training.py
@@ -28,8 +28,6 @@ def main():
     resume_from_previous = bool(int(sys.argv[3]))
     force_new_location = bool(int(sys.argv[4]))
 
-    # resume_from = '/ps/scratch/rdanecek/emoca/finetune_deca/2021_03_09_10-04-28_vaCoPhotoCoLMK_IDW-0.15_Aug_early'
-    # resume_from = '/ps/scratch/rdanecek/emoca/finetune_deca/2021_04_02_18-46-51_va_DeSegFalse_DeNone_Aug_DwC_early'
     resume_training(resume_from, start_at_stage=stage,
                     resume_from_previous=resume_from_previous, force_new_location=force_new_location)
 
