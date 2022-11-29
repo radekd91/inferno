Lip reading loss computation bug fix. Add the option for different lip reading metrics.

gdl/models/temporal/external/LipReadingLoss.py
@@ -114,8 +114,10 @@ class LipReadingNet(torch.nn.Module):
 
 class LipReadingLoss(torch.nn.Module):
 
-    def __init__(self, device):
+    def __init__(self, device, loss='cosine_similarity'):
         super().__init__()
+        self.loss = loss
+        assert loss in ['cosine_similarity', 'l1_loss', 'mse_loss']
         self.model = LipReadingNet(device)
         self.model.eval()
         # freeze model
@@ -144,13 +146,18 @@ class LipReadingLoss(torch.nn.Module):
             lip_features_pred = lip_features_pred[mask.view(-1)]
             # lip_features_gt = lip_features_gt[mask.squeeze(-1)]
             # lip_features_pred = lip_features_pred[mask.squeeze(-1)]
-
-        # # manual cosine similarity  take over from spectre
-        # lr = (lip_features_gt*lip_features_pred).sum(1)/torch.linalg.norm(lip_features_pred,dim=1)/torch.linalg.norm(lip_features_gt,dim=1)
-
-        # pytorch cosine similarity
-        lr = 1-torch.nn.functional.cosine_similarity(lip_features_gt, lip_features_pred, dim=1).mean()
-
-        loss = 1-torch.mean(lr)
-        return loss
+        
+        if self.loss == 'cosine_similarity':
+            # pytorch cosine similarity
+            lr = 1-torch.nn.functional.cosine_similarity(lip_features_gt, lip_features_pred, dim=1).mean()
+            ## manual cosine similarity  take over from spectre
+            # lr = (lip_features_gt*lip_features_pred).sum(1)/torch.linalg.norm(lip_features_pred,dim=1)/torch.linalg.norm(lip_features_gt,dim=1)
+            # lr = 1 - lr.mean()
+        elif self.loss == 'l1_loss':
+            lr = torch.nn.functional.l1_loss(lip_features_gt, lip_features_pred)
+        elif self.loss == 'mse_loss':
+            lr = torch.nn.functional.mse_loss(lip_features_gt, lip_features_pred)
+        else:
+            raise ValueError(f"Unknown loss function: {self.loss}")
+        return lr
 