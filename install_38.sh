c76c56393290d5cb89602bde7daf5549957d2ced
rdanecek <danekradek@gmail.com>
2023-02-13 20:41:55 +0100

    Update install related to the EMOCA 2.0 release

--- install_38.sh
+++ install_38.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
+echo "Pulling submodules"
+bash pull_submodules.sh
+echo "Installing mamba"
+conda install mamba -n base -c conda-forge
 if ! command -v mamba &> /dev/null
 then
     echo "mamba could not be found. Please install mamba before running this script"
     exit
 fi
-
-echo "Pulling submodules"
-bash pull_submodules.sh
-echo "Installing mamba"
-conda install mamba -n base -c conda-forge
 echo "Creating conda environment"
-mamba env create python=3.8 --file conda-environment_py38_cu11_ubuntu.yml 
+mamba create -n work38 python=3.8 
 eval "$(conda shell.bash hook)" # make sure conda works in the shell script
 conda activate work38
-echo "Installing GDL"
+if echo $CONDA_PREFIX | grep work38
+then
+    echo "Conda environment successfully activated"
+else
+    echo "Conda environment not activated. Probably it was not created successfully for some reason. Please activate the conda environment before running this script"
+    exit
+fi
+echo "Installing conda packages"
+mamba env update -n work38 --file conda-environment_py38_cu11_ubuntu.yml 
+echo "Installing other requirements"
+pip install -r requirements38.txt
 pip install Cython==0.29
-pip install -e . 
 echo "Making sure Pytorch3D installed correctly"
 pip install git+https://github.com/facebookresearch/pytorch3d.git@v0.6.2
-echo "Installing other requirements"
+echo "Installing GDL"
 pip install -e . 
-cd .. 
-pip install -r requirements38.txt
-cd ../..
 echo "Installation finished"
+
