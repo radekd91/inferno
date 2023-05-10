4724a67feb7c274ace77525d3dbc828611a0516a
rdanecek <danekradek@gmail.com>
2023-05-10 11:10:23 +0200

    Make video saving callback more efficient by not dumping images one by one but keep them in memory instead.

--- gdl/callbacks/TalkingHeadRenderingCallback.py
+++ gdl/callbacks/TalkingHeadRenderingCallback.py
@@ -27,6 +27,9 @@ class TalkingHeadTestRenderingCallback(pl.Callback):
         self.predicted_vertex_key = predicted_vertex_key or "predicted_vertices"
         self.path_chunks_to_cat = path_chunks_to_cat or 0
         self.save_meshes = save_meshes
+        self.save_frames_to_disk = False
+        self._image_cache = []
+        self._audio_cache = []
 
     def _path_chunk(self, video_name):
         video_name = Path(video_name)
@@ -113,7 +116,14 @@ class TalkingHeadTestRenderingCallback(pl.Callback):
                 # concatenate the images
                 image = np.concatenate([gt_image, pred_image], axis=1)
 
-                imsave(image_path, image)
+                # if trainer.datamodule.sequence_length_test != "all" or not self.save_images_to_disk:
+                # if trainer.datamodule.sequence_length_test != "all" or not self.save_images_to_disk:
+                if self.save_frames_to_disk:
+                    imsave(image_path, image)
+                else: 
+                    if not hasattr(self, "_image_cache"):
+                        self._image_cache = []
+                    self._image_cache += [image]
 
                 if self.save_meshes: 
                     import trimesh
@@ -121,12 +131,25 @@ class TalkingHeadTestRenderingCallback(pl.Callback):
                     mesh_path = path / (self.image_format % frame_index + ".obj")
                     mesh.export(mesh_path)
 
+                raw_audio_chunk = batch["raw_audio"][b,t].detach().cpu().numpy()
+                if self.save_frames_to_disk:
+                    if 'raw_audio' in batch.keys():
+                        audio_chunk_path = path / (self.image_format % frame_index + ".pkl")
+                        with open(audio_chunk_path, "wb") as f:
+                            pkl.dump(raw_audio_chunk, f)
+                else: 
+                    if not hasattr(self, "_audio_cache"):
+                        self._audio_cache = []
+                    self._audio_cache += [raw_audio_chunk]
 
-                if 'raw_audio' in batch.keys():
-                    raw_audio_chunk = batch["raw_audio"][b,t].detach().cpu().numpy()
-                    audio_chunk_path = path / (self.image_format % frame_index + ".pkl")
-                    with open(audio_chunk_path, "wb") as f:
-                        pkl.dump(raw_audio_chunk, f)
+            if not self.save_frames_to_disk and len(self._image_cache) > 0:
+                # write the video
+                # self._compose_chunked_audio(path, samplerate=self.audio_samplerates_to_process[path])
+                self._compose_chunked_audio(path, samplerate=self.audio_samplerates_to_process[path], audio_chunks=self._audio_cache)
+                self._save_video_from_tensor(self._image_cache, self.video_framerates_to_process[path], path)
+
+                self._image_cache = []
+                self._audio_cache = []
 
             # self._create_video(path, logger, trainer.global_step)
 
@@ -134,6 +157,31 @@ class TalkingHeadTestRenderingCallback(pl.Callback):
     #     super().on_test_epoch_begin(trainer, pl_module)
     #     self._create_videos(trainer, pl_module))
 
+    def _save_video_from_tensor(self, image_list, framerate, path): 
+        import skvideo
+        image_tensor = np.stack(image_list)
+        audio_path = path / "audio.wav"
+        video_path = path / "output_no_audio.mp4"
+        # write the video including audio 
+
+        skvideo.io.vwrite(video_path, image_tensor, 
+                          inputdict={'-r': str(framerate)}, 
+                          outputdict={'-r': str(framerate)})
+        
+        # combine the audio and video using ffmpeg
+        if audio_path.is_file():
+            ffmpeg_cmd = f"ffmpeg -y -i {str(video_path)} -i {str(audio_path)} -c:v copy -c:a aac -strict experimental {str(path / 'output.mp4')}"
+            os.system(ffmpeg_cmd)
+
+            # remove the video without audio
+            video_path.unlink()
+            # remove the audio file
+        else: 
+            video_path.rename(path / "output.mp4")
+
+        # remove the audio file
+        audio_path.unlink()
+        
 
     def on_test_epoch_end(self, trainer, pl_module):
         super().on_test_epoch_end(trainer, pl_module)
@@ -151,7 +199,25 @@ class TalkingHeadTestRenderingCallback(pl.Callback):
         self.video_conditions = {}
         self.dl_names = {}
 
+    def _compose_chunked_audio(self, subfolder, samplerate, audio_chunks=None):
+        # create audio 
+        audio_chunks_files = None
+        if audio_chunks is None:
+            audio_chunks_files = sorted([f for f in subfolder.iterdir() if f.is_file() and f.suffix == ".pkl"])
+            audio_chunks = [pkl.load(open(f, "rb")) for f in audio_chunks_files]
 
+        if len(audio_chunks) > 0:
+            audio_chunks = np.concatenate(audio_chunks, axis=0)
+            audio_path = subfolder / "audio.wav"
+            sf.write(audio_path, audio_chunks, samplerate)
+        else:
+            audio_path = None
+
+        if audio_path.is_file() and audio_chunks_files is not None:
+            # delete the audio chungs 
+            for audio_chunk in audio_chunks_files:
+                os.remove(audio_chunk)
+        return audio_path
 
     def _create_video(self, subfolder, logger, epoch):
         # find audio
@@ -166,14 +232,15 @@ class TalkingHeadTestRenderingCallback(pl.Callback):
             renderings = sorted([f for f in subfolder.iterdir() if f.is_file() and f.suffix == ".png"])    
 
             # create audio 
+            audio_path = self._compose_chunked_audio(subfolder, samplerate)
             audio_chunks_files = sorted([f for f in subfolder.iterdir() if f.is_file() and f.suffix == ".pkl"])
-            if len(audio_chunks_files) > 0:
-                audio_chunks = [pkl.load(open(f, "rb")) for f in audio_chunks_files]
-                audio_chunks = np.concatenate(audio_chunks, axis=0)
-                audio_path = subfolder / "audio.wav"
-                sf.write(audio_path, audio_chunks, samplerate)
-            else:
-                audio_path = None
+            # if len(audio_chunks_files) > 0:
+            #     audio_chunks = [pkl.load(open(f, "rb")) for f in audio_chunks_files]
+            #     audio_chunks = np.concatenate(audio_chunks, axis=0)
+            #     audio_path = subfolder / "audio.wav"
+            #     sf.write(audio_path, audio_chunks, samplerate)
+            # else:
+            #     audio_path = None
 
             # create video with audio
             if audio_path is not None:
@@ -198,9 +265,6 @@ class TalkingHeadTestRenderingCallback(pl.Callback):
             for rendering in renderings:
                 os.remove(rendering)
             
-            # delete the audio chungs 
-            for audio_chunk in audio_chunks_files:
-                os.remove(audio_chunk)
 
             if audio_path is not None:
                 os.remove(audio_path)
