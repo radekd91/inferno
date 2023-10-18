511f6cfa22ce840cc28ec0de2c5b83aa1cffb42e
rdanecek <danekradek@gmail.com>
2023-10-18 19:53:22 +0200

    Remove commented code

--- gdl_apps/TalkingHead/utils/load.py
+++ gdl_apps/TalkingHead/utils/load.py
@@ -27,58 +27,23 @@ from gdl.models.IO import locate_checkpoint
 
 def load_model(path_to_models,
               run_name,
-            #   stage,
-            #   relative_to_path=None,
-            #   replace_root_path=None,
-              mode='best',
-            #   allow_stage_revert=False, # allows to load coarse if detail checkpoint not found
+              mode='latest',
               ):
     run_path = Path(path_to_models) / run_name
     with open(Path(run_path) / "cfg.yaml", "r") as f:
         cfg = OmegaConf.load(f)
 
-    # conf = replace_asset_dirs(conf, Path(path_to_models) / run_name)
-    # conf.coarse.checkpoint_dir = str(Path(path_to_models) / run_name / "coarse" / "checkpoints")
-    # conf.coarse.full_run_dir = str(Path(path_to_models) / run_name / "coarse" )
-    # conf.coarse.output_dir = str(Path(path_to_models) )
-    # conf.detail.checkpoint_dir = str(Path(path_to_models) / run_name / "detail" / "checkpoints")
-    # conf.detail.full_run_dir = str(Path(path_to_models) / run_name / "detail" )
-    # conf.detail.output_dir = str(Path(path_to_models) )
-    faceformer = load_faceformer(cfg,
-              mode,
-            #   relative_to_path,
-            #   replace_root_path,
-            #   terminate_on_failure= not allow_stage_revert
-              )
-    # if faceformer is None and allow_stage_revert:
-    #     faceformer = load_faceformer(conf,
-    #                      mode,
-    #                     #  relative_to_path,
-    #                     #  replace_root_path,
-    #                      )
-
+    faceformer = load_faceformer(cfg, mode,)
     return faceformer, cfg
-
 
 
 def load_faceformer(
               cfg,
-            #   stage,
               mode,
               relative_to_path=None,
               replace_root_path=None,
               terminate_on_failure=True,
               ):
-    # print(f"Taking config of stage '{stage}'")
-    # print(conf.keys())
-    # if stage is not None:
-    #     cfg = conf[stage]
-    # else:
-    #     cfg = conf
-    # if relative_to_path is not None and replace_root_path is not None:
-    #     cfg = hack_paths(cfg, replace_root_path=replace_root_path, relative_to_path=relative_to_path)
-    # cfg.model.resume_training = False
-
     checkpoint = locate_checkpoint(cfg, replace_root_path, relative_to_path, mode=mode)
     if checkpoint is None:
         if terminate_on_failure:
@@ -86,9 +51,6 @@ def load_faceformer(
         else:
             return None
     print(f"Loading checkpoint '{checkpoint}'")
-    # if relative_to_path is not None and replace_root_path is not None:
-    #     cfg = hack_paths(cfg, replace_root_path=replace_root_path, relative_to_path=relative_to_path)
-
     checkpoint_kwargs = {
         "cfg": cfg
     }
