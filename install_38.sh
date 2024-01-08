dd073040a7fc84ed83fd7b61d047dfe47c1f1e79
rdanecek <danekradek@gmail.com>
2024-01-08 03:50:26 +0100

    Fix typi in install script. Add automatic download of some insightface models.

--- install_38.sh
+++ install_38.sh
@@ -21,7 +21,7 @@ else
 fi
 echo "Installing conda packages"
 # mamba env update -n work38 --file conda-environment_py38_cu11_ubuntu.yml 
-mamba env update -n work38 --file conda-environment_py38_cu11.yml 
+mamba env update -n work38 --file conda-environment_py38_cu11.yaml 
 echo "Installing other requirements"
 pip install -r requirements38.txt
 pip install Cython==0.29
@@ -29,5 +29,18 @@ echo "Making sure Pytorch3D installed correctly"
 pip install git+https://github.com/facebookresearch/pytorch3d.git@v0.6.2
 echo "Installing INFERNO"
 pip install -e . 
+
+# Insightface has problems with downloading some of their models
+echo -e "\nDownloading insightface models..."
+mkdir -p ~/.insightface/models/
+if [ ! -d ~/.insightface/models/antelopev2 ]; then
+  wget -O ~/.insightface/models/antelopev2.zip "https://keeper.mpdl.mpg.de/f/2d58b7fed5a74cb5be83/?dl=1"
+  unzip ~/.insightface/models/antelopev2.zip -d ~/.insightface/models/antelopev2
+fi
+if [ ! -d ~/.insightface/models/buffalo_l ]; then
+  wget -O ~/.insightface/models/buffalo_l.zip "https://keeper.mpdl.mpg.de/f/8faabd353cfc457fa5c5/?dl=1"
+  unzip ~/.insightface/models/buffalo_l.zip -d ~/.insightface/models/buffalo_l
+fi
+
 echo "Installation finished"
 
