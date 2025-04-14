ca17df6db00bba65fda691f3f2b01071744546e2
Symeonidis-Herzig, Alexandre (PG/R - Comp Sci & Elec Eng) <a.symeonidisherzig@surrey.ac.uk>
2025-04-14 15:59:36 +0100

    Fixing docker run

--- docker/work38/run_docker.sh
+++ docker/work38/run_docker.sh
@@ -4,12 +4,12 @@
 if [ $# -eq 0 ]
 then
     ## run docker with the codebase mounted
-    docker run -it --gpus all -v $PWD/..:/workspace/repos/inferno inferno_docker:test /bin/bash
+    docker run -it --gpus all -v $PWD/..:/workspace/repos/inferno inferno_docker /bin/bash
     # docker run -it --gpus all -v $PWD/..:/workspace/repos/inferno inferno_docker_egl /bin/bash
     # docker run -it --gpus all -v $PWD/..:/workspace/repos/inferno inferno_docker_cudagl /bin/bash
 else
     ## run docker with the codebase mounted and execute the command
-    docker run -it --gpus all -v $PWD/..:/workspace/repos/inferno inferno_docker:test $@
+    docker run -it --gpus all -v $PWD/..:/workspace/repos/inferno inferno_docker $@
     # docker run -it --gpus all -v $PWD/..:/workspace/repos/inferno inferno_docker_egl $@
     # docker run -it --gpus all -v $PWD/..:/workspace/repos/inferno inferno_docker_cudagl $@
 fi
