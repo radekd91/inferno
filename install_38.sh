a02aef08eb356c287e6e2809271167a49c051c62
Alonso <alonsorobots@gmail.com>
2025-04-25 04:00:36 +0000

    Update installation scripts; add new dependencies and improve installation checks

--- install_38.sh
+++ install_38.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 echo "Pulling submodules"
 bash pull_submodules.sh
-echo "Installing mamba"
-conda install mamba -n base -c conda-forge
+echo "checking mamba environment"
+conda list -n base | grep -q '^mamba\s'
+if [ $? -ne 0 ]; then
+    echo "installing mamba"
+    conda install mamba -n base -c conda-forge
+else
+    echo "mamba is already installed in the base environment."
+fi
 if ! command -v mamba &> /dev/null
 then
     echo "mamba could not be found. Please install mamba before running this script"
     exit
 fi
 echo "Creating conda environment"
-mamba create -n work38 python=3.8 
+if ! conda env list | grep -q '^work38\s'; then
+    mamba create -n work38 python=3.8
+else
+    echo "Conda environment 'work38' already exists."
+fi
 eval "$(conda shell.bash hook)" # make sure conda works in the shell script
 conda activate work38
 if echo $CONDA_PREFIX | grep work38
@@ -20,11 +30,26 @@ else
     exit
 fi
 echo "Installing conda packages"
+echo "downloading legacy wheel files"
+if ! conda list -n work38 | grep -q '^gdown\s'; then
+    echo "gdown is not installed. Installing gdown using pip..."
+    conda activate work38 && pip install gdown
+else
+    echo "gdown is already installed."
+fi
+echo "downloading mediapipe wheel"
+gdown 1N-nW3gJMst8vO2qRkF1VFoXCOEqhbos1
+echo "Installing mediapipe wheel manually..."
+pip install mediapipe-0.8.10-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+echo "downloading onnxruntime-gpu wheel"
+gdown 1QyrrLLoIlWx-o9rovfPNC84ixQugEwSh
+echo "Installing onnxruntime-gpu wheel manually..."
+pip install onnxruntime_gpu-1.9.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
 # mamba env update -n work38 --file conda-environment_py38_cu11_ubuntu.yml 
+pip install Cython==0.29.14
 mamba env update -n work38 --file conda-environment_py38_cu11.yaml 
 echo "Installing other requirements"
 pip install -r requirements38.txt
-pip install Cython==0.29
 echo "Making sure Pytorch3D installed correctly"
 pip install git+https://github.com/facebookresearch/pytorch3d.git@v0.6.2
 echo "Installing INFERNO"
